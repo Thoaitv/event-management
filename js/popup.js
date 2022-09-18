9e6a2eca5cc4e51626b64b424ee44bad86d044b5
Thoai <thoaitavan@gmail.com>
2022-09-19 01:43:53 +0700

    prepare add event

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,97 @@
 let closeBut = document.getElementsByClassName('close')[0]
 let modal = document.getElementsByClassName('modal-cont')[0]
 let delegate = document.getElementsByClassName('delegate')[0]
+let box = document.getElementsByClassName('box')[0]
 
 //close
 function x() {
     modal.style.display = "none";
+    // box.style.display = "block";
+
 }
 closeBut.onclick = x;
 
 delegate.onclick = function () {
     modal.style.display = "block";
+    // box.style.display = "none";
 }
-
 window.onclick = function (e) {
     if (e.target.className === 'modal-cont') {
         e.target.style.display = 'none';
+        // box.style.display = "block";
+
     }
 }
 function preview() {
     frame.src = URL.createObjectURL(event.target.files[0]);
+}
+
+// QR
+let closeBtnQR = document.getElementsByClassName('close-qr')[0]
+let modalQR = document.getElementsByClassName('modal-cont-qr')[0]
+let btnQr = document.getElementsByClassName('btn-qr')[0]
+//close
+
+function qr() {
+
+    modalQR.style.display = "none";
+}
+
+closeBtnQR.onclick = qr;
+
+btnQr.onclick = function () {
+    console.log(modalQR.style.display)
+
+    modalQR.style.display = "block";
+}
+window.onclick = function (e) {
+    if (e.target.className === 'modal-cont-qr') {
+        e.target.style.display = 'none';
+    }
+}
+// Profile
+let closeBtnProfile = document.getElementsByClassName('close-profile')[0]
+let modalProfile = document.getElementsByClassName('modal-cont-profile')[0]
+let btnProfile = document.getElementsByClassName('btn-profile')[0];
+let container = document.getElementsByClassName('modal-cont-profile')[0];
+//close
+function profile() {
+    modalProfile.style.display = "none";
+}
+closeBtnProfile.onclick = profile;
+console.log(container);
+btnProfile.onclick = function () {
+    console.log(modalProfile.style)
+    container.style.backgroundColor = "white"
+    modalProfile.style.display = "block";
+
+}
+window.onclick = function (e) {
+    if (e.target.className === 'modal-cont-profile') {
+        e.target.style.display = 'none';
+    }
+}
+
+
+// ADD EVENT
+
+let closeBtnAddEvent = document.getElementsByClassName('close-add-event')[0]
+let modalAddEvent = document.getElementsByClassName('modal-cont-add-event')[0]
+let btnAddEvent = document.getElementsByClassName('btn-add-event')[0];
+// let container = document.getElementsByClassName('modal-cont-profile')[0];
+//close
+function addEvent() {
+    modalAddEvent.style.display = "none";
+}
+closeBtnAddEvent.onclick = addEvent;
+console.log(container);
+btnAddEvent.onclick = function () {
+    console.log("A")
+    modalAddEvent.style.display = "block";
+
+}
+window.onclick = function (e) {
+    if (e.target.className === 'modal-cont-add-event') {
+        e.target.style.display = 'none';
+    }
 }
